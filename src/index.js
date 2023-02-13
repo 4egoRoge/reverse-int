e95a905c60eb66166535a010243182b2430dca60
4egoRoge <86154572+4egoRoge@users.noreply.github.com>
2023-02-13 16:41:06 +0500

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res
+    if(n < 0) {
+        n = n * -1
+        res = String(n).split('').reverse().join('')
+    } else {
+        res = String(n).split('').reverse().join('')
+    }
+    return res
 }
